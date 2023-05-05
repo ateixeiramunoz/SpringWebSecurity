c3ca1b49e1084b4a0c111954b526306554e22644
Profesor <mail.alejandro.teixeira@gmail.com>
2023-05-05 03:14:03 +0200

    Añadida primera version de Gitlab-CI

--- src/main/java/com/eoi/springwebsecurity/email/EmailService.java
+++ src/main/java/com/eoi/springwebsecurity/email/EmailService.java
@@ -1,40 +1,36 @@
 package com.eoi.springwebsecurity.email;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
-
 @Service
+@Log4j2
 public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
 
-
+/*
     public void sendEmail(String recipientEmail, String link, String cuerpoMensaje)
             throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
-
         helper.setFrom("cuentadepruebaspringboot@gmail.com", "Soporte EOI");
         helper.setTo(recipientEmail);
-
         String subject = "Tienes un aviso";
-
         String content = "<p>Hola,</p>"
                 + "<p>Tienes un aviso de la app</p>";
-
         helper.setSubject(subject);
-
         helper.setText(content, true);
 
         mailSender.send(message);
-    }
+    }*/
 
+    public void sendEmail(String recipientEmail, String link, String cuerpoMensaje)
+    {
+        log.info("HEMOS ENVIADO EL MAIL PERO NJO FUNCIONA EL ENVIO!!");
+    }
 
 }
